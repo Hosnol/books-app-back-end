54833f41ea565219b5d599de730476bb88da33f9
Hosnol Arifin <hosnolarifin220@gmail.com>
2023-11-18 19:14:10 +0700

    update routes

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+    addBookHandler, 
+    getAllBookHandler, 
+    getBookByIdHandler, 
+    editBookByIdHandler, 
+    deleteBookByIdHandler
+} = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllBookHandler
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getBookByIdHandler
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editBookByIdHandler
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteBookByIdHandler
+    }
+];
+
+module.exports = routes;
